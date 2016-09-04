f75f037748cdc5728ebf8813f1dd5d4f15fee667
mrexodia <mr.exodia.tpodt@gmail.com>
2016-09-04 07:11:58 +0200

    finished challenge_1_7

---
+++ challenge_1_7/challenge_1_7.cpp
@@ -0,0 +1,29 @@
+#include "../challenge_1_1/challenge_1_1.h"
+#include "../challenge_1_4/challenge_1_4.h"
+#include "challenge_1_7.h"
+
+int main()
+{
+    std::string base64;
+    if (!FileReadAllNoWhitespaceText("7.txt", base64))
+        puts("FileReadAllNoWhitespaceText failed...");
+    else
+    {
+        std::vector<unsigned char> data;
+        if (!FromBase64(base64, data))
+            puts("FromBase64 failed...");
+        else
+        {
+            std::vector<unsigned char> decrypted;
+            if (!aes_ecb_decrypt(data, StringToVector("YELLOW SUBMARINE"), decrypted))
+                puts("aes_ecb_decrypt failed...");
+            else
+            {
+                RemovePKCS7Padding(decrypted);
+                puts(VectorToString(decrypted).c_str());
+            }
+        }
+    }
+    getchar();
+    return 0;
+}
---
+++ challenge_1_7/challenge_1_7.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "aes.h"
+#include "../challenge_1_6/challenge_1_6.h"
+
+inline bool aes_ecb_decrypt(const std::vector<unsigned char> & data, const std::vector<unsigned char> & key, std::vector<unsigned char> & out)
+{
+    if (key.size() != 16)
+        return false;
+    
+    out.reserve(data.size());
+
+    WORD aeskey[60];
+    std::vector<unsigned char> aesin, aesout;
+    aesout.resize(AES_BLOCK_SIZE);
+    const auto aeskeysize = 128;
+    aes_key_setup(key.data(), aeskey, aeskeysize);
+
+    auto blocks = data.size() / AES_BLOCK_SIZE;
+    for (size_t i = 0; i < blocks; i++)
+    {
+        if (!TakeBytes(data, i * AES_BLOCK_SIZE, AES_BLOCK_SIZE, aesin))
+        {
+            puts("TakeBytes failed...");
+            return false;
+        }
+        aes_decrypt(aesin.data(), aesout.data(), aeskey, aeskeysize);
+        for (size_t j = 0; j < aesout.size(); j++)
+            out.push_back(aesout[j]);
+    }
+
+    return true;
+}
+
+inline void RemovePKCS7Padding(std::vector<unsigned char> & plaintext, int blocksize = AES_BLOCK_SIZE)
+{
+    if (!plaintext.size())
+        return;
+    auto padsize = plaintext[plaintext.size() - 1];
+    if (padsize >= blocksize)
+        return;
+    for (size_t i = 0; i < padsize; i++)
+        if (plaintext[plaintext.size() - 1 - i] != padsize)
+            return;
+    plaintext.resize(plaintext.size() - padsize);
+}
